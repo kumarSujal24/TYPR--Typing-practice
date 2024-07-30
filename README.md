5305310e50eae169d9383f085be1f2a14f52ebc5
kumarSujal24 <kumarsujal186cse@gmail.com>
2024-07-30 18:52:03 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+Typr - Typing Practice
+Welcome to Typr, your typing practice companion!
+
+Overview:
+Typr is a simple web-based typing practice tool designed to help you improve your typing skills. 
+It provides a user-friendly interface with a typing challenge and various features to enhance your typing experience.
+
+Features:-
+
+Start Typing
+-Click the "Start" button to begin your typing practice session.
+-The tool will generate a random paragraph for you to type.
+  
+Timer
+
+-Keep an eye on the timer to track your typing speed and accuracy.
+-The timer displays minutes and seconds, showing how long you've been typing.
+
+Errors:-
+
+-The tool tracks the number of errors you make while typing.
+-Each mistake increments the error count.
+
+Evaluation:-
+
+-After completing the typing challenge, you'll see an evaluation section.
+-It provides your typing speed (words per minute) and accuracy score.
+
+Keyboard Display:-
+
+-A virtual keyboard is displayed to help you visualize key placements.
+-Keys highlight briefly when pressed.
+-Responsive Design
+
+Typr's interface is responsive, making it suitable for various screen sizes.
+How to Use
+-Click the "Start" button to begin typing.
+-Type the displayed paragraph in the input area.
+-Pay attention to the timer, errors, and the virtual keyboard.
+-After completion, review your typing speed and accuracy.
+
+Customization:-
+-Feel free to modify the design and styles in the provided HTML and CSS files.
+-The JavaScript code handles typing logic, timers, and error tracking.
+
+Credits
+Made with ❤️ by Kumar Sujal
+
+Notes
+-The paragraph for typing is fetched randomly from an API.
+-If there's an issue with the API, a default paragraph about cosmology is used.
+
+How to Run Locally
+Download the project files: HTML, CSS, and JavaScript.
+Open the HTML file in a web browser.
+Start typing and enjoy your practice!
+
+Feel free to explore, modify, and use Typr to enhance your typing skills!
---
+++ index.js
@@ -0,0 +1,250 @@
+let content = [];
+// window.addEventListener('load', getData, false )
+async function getData(){
+    let minLength = 100;
+    content = [];
+    try{
+	const resp = await fetch("https://random-word-api.vercel.app/api?words=20&length=5");
+    	const data = await resp.json();
+    	console.log(data);
+    	content = [data.join(" ")];
+        console.log(content)
+    }
+    catch(err){
+	    console.log(err);
+	    content =  ["Cosmology deals with the world as the totality of space, time and all phenomena. Historically, it has had quite a broad scope, and in many cases was founded in religion. In modern use metaphysical cosmology addresses questions about the Universe which are beyond the scope of science."];
+    }
+    finally{
+    	console.log(content);
+    }
+}
+
+getData();
+ 
+
+let para = document.querySelector("#content-to-type");
+let start = document.querySelector("#start");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
+let keys = document.querySelectorAll(".key");
+let inputContent = document.querySelector("#input-content");
+let errorVal = document.querySelector("#error-val");
+let evaluation = document.querySelector(".evaluation");
+let speedVal = document.querySelector("#speed-val");
+let scoreVal = document.querySelector("#score-val");
+let letters,typingLetterErrors=0;
+
+
+// let dataKeys = [];
+// for(let i = 0;i<keys.length;i++) {
+//     dataKeys.push(keys.item(i).attributes[1].value);
+// }
+
+//keydown event listener 
+document.addEventListener("keydown",keyPress);
+
+//updated input value event listener
+inputContent.addEventListener("input",inputChange)
+
+//start typing button event listener
+start.addEventListener("click", loadContent,true);
+
+
+//keypress animation 
+function keyPress(event){
+    let keyboardCode = event.keyCode;
+    if(keyboardCode>=48&&keyboardCode<=57) {
+        //inputContent.removeEventListener("input",inputChange);
+        // inputContent.innerHTML = inputContent.innerHTML.substr(0,inputContent.length-2)
+        console.log(keyboardCode)
+        //inputContent.addEventListener("input",inputChange)
+    }
+    else {
+    keyboardCode+=32;
+    
+    for(let i = 0;i<keys.length;i++) {
+        if(parseInt(keys.item(i).attributes[1].value)===keyboardCode){
+             //console.log(keys.item(i).attributes[1].value)
+             keys.item(i).classList.add("pressed");
+             setTimeout(()=>{
+                keys.item(i).classList.remove("pressed"); 
+             },100)
+             break;
+        }
+    }
+}  
+}
+
+function inputChange(event){
+    let inputVal = inputContent.value;
+    //.classList.remove("current");
+
+    //finding the integer equivalent of the last input character
+    let intVal = parseInt(inputContent.value.charAt(inputContent.value.length-1));
+
+    //if integer value is not NaN i.e. it is a number, remove it from the input value
+    if(!Number.isNaN(intVal)) inputContent.value = inputVal.substr(0,inputContent.value.length-1);
+    console.log(inputContent.value)
+    let keyCode = inputVal.charCodeAt(inputVal.length-1);
+    if(inputVal.length<=para.length-1)
+   startBlinking(inputVal.length)
+    checkInput(keyCode,inputVal);
+}
+function startBlinking(letterId){
+
+        window.blinkingId = setInterval(()=>{
+            blinking(letters.item(letterId));
+        },150);
+}
+function clearBlinking(){
+    clearInterval(window.blinkingId);
+}
+function blinking(letter){
+    if(letter.classList[1]==="current")
+        letter.classList.remove("current");
+        else {
+            letter.classList.add("current");
+        }
+}
+
+//letters.item(inputValue.length-1).innerHTML.charAt(0)==="_"&& inputValue ||
+
+function checkInput(keyCode,inputValue){
+    if(inputValue.length===letters.length){
+        pauseClock("Pause");
+       let [minsCompleted,secsCompleted] = getCurrentTimerValue(minutes.innerHTML,seconds.innerHTML);
+       let totalCorrect = letters.length - typingLetterErrors;
+       let percentageCorrect = Math.floor(totalCorrect/letters.length * 100);
+       let charsPerSecond = Math.floor(totalCorrect/(minsCompleted*60 + secsCompleted));
+       let wordsPerMinute = Math.floor(charsPerSecond*60)/10;
+       speedVal.innerHTML = wordsPerMinute+" words/min";
+       scoreVal.innerHTML = percentageCorrect;
+       start.innerHTML = "Restart";
+       //start.removeEventListener("click",loadContent,true)
+       console.log("Percentage =" +percentageCorrect)
+       console.log("Speed "+charsPerMinute+ " per minute")
+    }
+    else if(inputValue.length<letters.length){
+    if(keyCode>=97 && keyCode<=122 || keyCode===32){
+        if(keyCode===32) keyCode = 95;
+       if(letters.item(inputValue.length-1).innerHTML.charCodeAt(0)===keyCode){
+            //console.log(letters.item(inputValue.length-1),inputValue)
+            clearBlinking();
+            startBlinking(inputValue.length);
+            
+            letters.item(inputValue.length-1).classList.add("typed");
+        }
+        else {
+            typingLetterErrors++;
+            errorVal.innerHTML = typingLetterErrors;
+            clearBlinking();
+            startBlinking(inputValue.length);
+            letters.item(inputValue.length-1).classList.add("wrong");
+            console.log("Errors = "+typingLetterErrors)
+        }
+    }
+}
+}
+
+
+    //event.key.charCodeAt(0);
+
+    //console.log(inputVal.charCodeAt(inputVal.length-1))
+    //checkCurrentInput(event.key,para.innerHTML.charAt(0)      
+
+
+function loadContent(){
+    if(start.innerHTML==="Restart"){
+        window.location.reload();
+    }
+    inputContent.focus();
+    if(start.innerHTML==="Start"){
+        setNewContent();
+    }
+    else if (start.innerHTML==="Resume"){
+        startClock(seconds.innerHTML,minutes.innerHTML,start.innerHTML);//resume timer
+        start.innerHTML = "Pause";
+    }
+    else if (start.innerHTML === "Pause"){
+        pauseClock(start.innerHTML); //pausing the clock
+        start.innerHTML="Resume";
+    }
+}
+
+function setNewContent(){
+    typingLetterErrors=0;
+        let paraNumber = randomParaGenerator(1);
+        let wordsArray = wordSeparator(content[0].toLocaleLowerCase());
+        let typingPara =" ";
+        wordsArray = wordsArray.filter((word)=> word.length>=1)
+        //limiting to 25 words 
+        let letterId = 0;
+        for(let wordIndex =0;wordIndex<20;wordIndex++){
+            let word = wordsArray[wordIndex];
+            typingPara = typingPara+letterSeparator(word,letterId);
+            letterId = letterId+word.length+1;
+        }
+        
+        para.innerHTML = typingPara.trim();
+        letters = document.querySelectorAll(".typing");
+        startClock(seconds.innerHTML,minutes.innerHTML,start.innerHTML);//start timer
+        start.innerHTML = "Pause";
+}
+
+
+function wordSeparator(content){
+    return content.split(/[,. -_]/)
+}
+
+
+function letterSeparator(content,id){
+    let wordSpan="";
+    console.log(typeof content)
+    for(let charIndex=0;charIndex<content.length;charIndex++){
+        wordSpan= wordSpan + `<div class="typing" id="${id}">${content.charAt(charIndex)}</div>`;
+        id++;
+    }
+    wordSpan.trim();
+    wordSpan= wordSpan + `<div class="typing" id="${id}">_</div>`
+    return wordSpan;
+}
+function randomParaGenerator(limit){
+    return Math.floor(Math.random()*limit);
+}
+function startClock(secs,mins,condition){
+    let timerVals = getCurrentTimerValue(mins,secs);
+    let [minsVal,secsVal] = [...timerVals];
+
+    //After every second, check the value of condition
+    //If it is start, call the increase seconds function
+    //IF not, do nothing
+
+    startBlinking(inputContent.value.length)
+    //using window object because using local variables result in error in pausing the clock because of scoping
+     window.timerClockID = setInterval(() => { 
+        if(condition==="Start"|| condition==="Resume"){
+            secsVal = increaseSeconds(secsVal);
+            if(secsVal>59){
+                secsVal = 0;
+                minsVal=minsVal+1;
+            }
+            seconds.innerHTML = secsVal;
+            minutes.innerHTML = minsVal;
+        }
+    }, 1000);
+}  
+
+function getCurrentTimerValue(mins,secs){
+    let secsVal = parseInt(secs);
+    let minsVal = parseInt(mins);
+    let timer = [minsVal,secsVal];
+    return timer;
+}
+function increaseSeconds(secondsValue){
+    return secondsValue+1;
+}
+function pauseClock(condition){
+    if(condition==="Pause"){
+        clearInterval(window.timerClockID)
+    }
+}
